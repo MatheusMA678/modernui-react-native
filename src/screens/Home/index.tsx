98bfab16938df28ca5c8fb7b35953a9700fc81da
Matheus Mattos Avelino <matheusma2510@hotmail.com>
2022-11-17 04:00:39 -0400

    initial commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container, Header } from './styles';
+
+import { Input } from '../../components/Input';
+
+import coverImg from '../../assets/cover.png';
+
+export function Home() {
+  return(
+    <Container>
+      <Header source={coverImg}>
+        <Input placeholder='Pesquisar...' />
+      </Header>
+    </Container>
+  );
+}
---
+++ src/theme/index.ts
@@ -0,0 +1,15 @@
+export default {
+  COLORS: {
+    BACKGROUND: '#F2F2F2',
+    TEXT: '#170126',
+    ALERT: '#73074D',
+
+    PRIMARY_900: '#280540',
+    PRIMARY_800: '#3B0C59'
+  },
+  FONTS: {
+    REGULAR: 'Poppins_400Regular',
+    MEDIUM: 'Poppins_500Medium',
+    BOLD: 'Poppins_700Bold'
+  }
+}
